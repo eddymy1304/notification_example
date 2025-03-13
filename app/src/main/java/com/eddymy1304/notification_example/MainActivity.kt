324bc4f2001207bec6a017b63cabb8aca4d68375
Eddy Mendoza <eddymy1304@gmail.com>
2025-03-12 23:44:50 -0500

    create advanced notification

--- app/src/main/java/com/eddymy1304/notification_example/MainActivity.kt
+++ app/src/main/java/com/eddymy1304/notification_example/MainActivity.kt
@@ -1,12 +1,11 @@
 package com.eddymy1304.notification_example
 
-import android.Manifest
+import android.app.AlarmManager
 import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
-import android.content.pm.PackageManager
 import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
@@ -19,10 +18,9 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.core.app.ActivityCompat
-import androidx.core.app.NotificationCompat
-import androidx.core.app.NotificationManagerCompat
+import com.eddymy1304.notification_example.ui.AlarmNotification
 import com.eddymy1304.notification_example.ui.theme.NotificationExampleTheme
+import java.util.Calendar
 
 class MainActivity : ComponentActivity() {
 
@@ -43,12 +41,31 @@ class MainActivity : ComponentActivity() {
                     verticalArrangement = Arrangement.Center
                 ) {
                     Button(
-                        onClick = { createSimpleNotification() }) {
+                        onClick = { scheduleNotification() }
+                    ) {
                         Text(text = "Simple Notification")
                     }
                 }
             }
         }
+    }
+
+    private fun scheduleNotification() {
+
+        val intent = Intent(applicationContext, AlarmNotification::class.java)
+        val pendingIntent = PendingIntent.getBroadcast(
+            applicationContext,
+            NOTIFICATION_ID,
+            intent,
+            PendingIntent.FLAG_IMMUTABLE or PendingIntent.FLAG_UPDATE_CURRENT
+        )
+
+        val alarmManager = getSystemService(Context.ALARM_SERVICE) as AlarmManager
+        alarmManager.set(
+            AlarmManager.RTC_WAKEUP,
+            Calendar.getInstance().timeInMillis + 10_000L,
+            pendingIntent
+        )
     }
 
     private fun createChannel() {
@@ -67,43 +84,4 @@ class MainActivity : ComponentActivity() {
         }
     }
 
-    private fun createSimpleNotification() {
-
-        val intent = Intent(this, MainActivity::class.java).apply {
-            flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
-        }
-
-        val pendingIntent = PendingIntent.getActivity(
-            this,
-            0,
-            intent,
-            PendingIntent.FLAG_IMMUTABLE
-        )
-
-        val builder = NotificationCompat
-            .Builder(this, CHANNEL_ID)
-            .setSmallIcon(R.drawable.outline_favorite_border_24)
-            .setContentTitle(getString(R.string.title_notification))
-            .setStyle(
-                NotificationCompat
-                    .BigTextStyle()
-                    .bigText(getString(R.string.text_large_notification))
-                    .setSummaryText(getString(R.string.text_notification))
-            )
-            .setContentText(getString(R.string.text_notification))
-            .setContentIntent(pendingIntent)
-            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
-            .setAutoCancel(true)
-
-        with(NotificationManagerCompat.from(this)) {
-            if (ActivityCompat.checkSelfPermission(
-                    this@MainActivity,
-                    Manifest.permission.POST_NOTIFICATIONS
-                ) != PackageManager.PERMISSION_GRANTED
-            ) {
-                return@with
-            }
-            notify(NOTIFICATION_ID, builder.build())
-        }
-    }
 }
--- app/src/main/java/com/eddymy1304/notification_example/ui/AlarmNotification.kt
+++ app/src/main/java/com/eddymy1304/notification_example/ui/AlarmNotification.kt
@@ -1,11 +1,56 @@
 package com.eddymy1304.notification_example.ui
 
+import android.app.NotificationManager
+import android.app.PendingIntent
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import androidx.core.app.NotificationCompat
+import com.eddymy1304.notification_example.MainActivity
+import com.eddymy1304.notification_example.MainActivity.Companion.CHANNEL_ID
+import com.eddymy1304.notification_example.R
 
-class AlarmNotification: BroadcastReceiver() {
-    override fun onReceive(context: Context?, intent: Intent?) {
+class AlarmNotification : BroadcastReceiver() {
 
+    companion object {
+        const val NOTIFICATION_ID = 1
+    }
+
+    override fun onReceive(context: Context, intent: Intent?) {
+        createSimpleNotification(context)
+    }
+
+
+    private fun createSimpleNotification(context: Context) {
+
+        val intent = Intent(context, MainActivity::class.java).apply {
+            flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+        }
+
+        val pendingIntent = PendingIntent.getActivity(
+            context,
+            0,
+            intent,
+            PendingIntent.FLAG_IMMUTABLE
+        )
+
+        val notification = NotificationCompat
+            .Builder(context, CHANNEL_ID)
+            .setSmallIcon(R.drawable.outline_favorite_border_24)
+            .setContentTitle(context.getString(R.string.title_notification))
+            .setStyle(
+                NotificationCompat
+                    .BigTextStyle()
+                    .bigText(context.getString(R.string.text_large_notification))
+                    .setSummaryText(context.getString(R.string.text_notification))
+            )
+            .setContentText(context.getString(R.string.text_notification))
+            .setContentIntent(pendingIntent)
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+            .setAutoCancel(true)
+            .build()
+
+        val manager = context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        manager.notify(NOTIFICATION_ID, notification)
     }
 }
